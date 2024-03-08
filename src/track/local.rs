cd394db4ef561a972d673b0bf126896f871bf125
iceice666 <iceice666@outlook.com>
2024-03-08 15:39:18 +0800

    feat(track): impl local track

--- src/track/local.rs
+++ src/track/local.rs
@@ -1,61 +1,46 @@
+use async_trait::async_trait;
 use rodio::Decoder;
-use std::{collections::HashMap, fs::File, io::BufReader, path::Path};
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::BufReader,
+    path::{Path, PathBuf},
+};
 use tracing::instrument;
 
-use super::Track;
+use crate::track::Track;
 
 type AnyResult<T = ()> = anyhow::Result<T>;
 
 #[derive(Debug)]
-struct LocalTrack {
-    file_path: String,
+pub(crate) struct LocalTrack {
+    file_path: PathBuf,
 }
 
 impl LocalTrack {
     pub fn new(file_path: &str) -> LocalTrack {
         LocalTrack {
-            file_path: file_path.to_string(),
+            file_path: Path::new(file_path).to_path_buf(),
         }
     }
 }
 
+#[async_trait]
 impl Track for LocalTrack {
     type Source = BufReader<File>;
 
     #[instrument]
-    fn refresh(&self) -> AnyResult {
-        let path = Path::new(&self.file_path);
-
-        match path.exists() {
-            true => {
-                // Nothing to do here
-                Ok(())
-            }
-            false => Err(anyhow::anyhow!("File does not exist: {}", self.file_path)),
-        }
-    }
-
-    #[instrument]
-    fn get_decoded_source(&self) -> AnyResult<Decoder<Self::Source>> {
-        self.refresh()?;
-
+    async fn get_decoded_source(&self) -> AnyResult<Decoder<Self::Source>> {
         let path = File::open(&self.file_path)?;
         let buf = BufReader::new(path);
         let dec = Decoder::new(buf)?;
 
         Ok(dec)
     }
-
-    #[instrument]
-    fn get_metadata(&self) -> AnyResult<HashMap<String, String>> {
-        Ok(HashMap::new())
-    }
 }
 
 #[cfg(test)]
 mod test {
-    use rodio::Decoder;
-
     use rodio::{OutputStream, Sink};
 
     use tracing::info;
@@ -63,8 +48,8 @@ mod test {
     use super::*;
 
     type AnyResult<T = ()> = anyhow::Result<T>;
-    #[test]
-    fn file_path() -> AnyResult {
+    #[tokio::test]
+    async fn file_path() -> AnyResult {
         tracing_subscriber::fmt::init();
 
         let track = LocalTrack::new("assets/ViRTUS.m4a");
@@ -74,34 +59,21 @@ mod test {
         let sink = Sink::try_new(&stream_handle).unwrap();
         info!("New sink");
 
-        sink.set_volume(0.1);
+        sink.set_volume(1.0);
 
-        let dec = track.get_decoded_source()?;
-        info!("New decoded source");
+        loop {
+            let dec = track.get_decoded_source().await?;
+            info!("New decoded source");
 
-        sink.append(dec);
-        info!("Appended to sink");
+            sink.append(dec);
+            info!("Appended to sink");
 
-        sink.play();
-        info!("Playing");
+            sink.play();
+            info!("Playing");
 
-        sink.sleep_until_end();
-        info!("End");
-
-        std::thread::sleep(std::time::Duration::from_secs(5));
-        info!("Wait for 5 secs");
-
-        let dec = track.get_decoded_source()?;
-        info!("Prepare to play again");
-
-        sink.append(dec);
-        info!("Appended to sink");
-
-        sink.play();
-        info!("Playing");
-
-        sink.sleep_until_end();
-        info!("End");
+            sink.sleep_until_end();
+            info!("End");
+        }
 
         Ok(())
     }
--- src/track/mod.rs
+++ src/track/mod.rs
@@ -1,5 +1,8 @@
 mod local;
+mod remote;
+mod youtube;
 
+use async_trait::async_trait;
 use std::{
     collections::HashMap,
     io::{Read, Seek},
@@ -7,14 +10,13 @@ use std::{
 
 use rodio::Decoder;
 
+pub(crate) use local::LocalTrack;
+
 type AnyResult<T = ()> = anyhow::Result<T>;
 
+#[async_trait]
 pub(crate) trait Track {
     type Source: Read + Seek + Sync + Send + 'static;
 
-    fn refresh(&self) -> AnyResult;
-
-    fn get_decoded_source(&self) -> AnyResult<Decoder<Self::Source>>;
-
-    fn get_metadata(&self) -> AnyResult<HashMap<String, String>>;
+    async fn get_decoded_source(&self) -> AnyResult<Decoder<Self::Source>>;
 }
